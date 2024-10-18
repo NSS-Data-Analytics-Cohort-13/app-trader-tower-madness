80eb45b2a85ef383c39622f27dd1fc6014f0d5d7
Jswine08 <jswine08@gmail.com>
2024-10-17 21:38:44 -0500

    101724 938pm

--- Script/tower_madness_project.sql
+++ Script/tower_madness_project.sql
@@ -94,12 +94,61 @@ GROUP BY name,price
 ORDER BY avg_rating DESC
 LIMIT 12
 -------------------------------------------------------------------------
+--Philip crazy formula
+
+SELECT combined.name,
+	combined.price,
+	aps.primary_genre,
+	psa.install_count,
+	SUM(CAST(aps.review_count AS integer) + psa.review_count) AS review_count_all,
+ROUND(AVG(combined.rating),1) AS avg_rating,
+	CASE
+		 WHEN combined.price = 0 THEN combined.price + 10000
+		 WHEN combined.price <= 1.00 THEN  combined.price * 10000
+		 WHEN combined.price > 1.00 THEN combined.price * 10000
+		 END as purchase_price,
+		 9000 as monthly_revenue,
+	CASE
+		 WHEN ROUND(AVG(combined.rating),1) >= 4.7 and combined.price = 0.00
+		 THEN 'advisable purchase'
+		 ELSE 'not recommended'
+		 END as Purchase_recommendation
+FROM (
+    --app_store_apps
+    SELECT name,
+		   rating,
+		   CAST(price AS NUMERIC) AS price,
+		   primary_genre
+    FROM app_store_apps
+    UNION ALL
+    --  play_store_apps
+    SELECT name,
+		   rating,
+		   Cast(REPLACE(TRIM(price),'$','') as NUMERIC) as price,
+		   genres
+    FROM play_store_apps
+) AS combined
+JOIN app_store_apps aps ON combined.name=aps.name
+join play_store_apps psa ON combined.name=psa.name
+WHERE combined.rating IS NOT NULL
+GROUP BY combined.name,
+         combined.price,
+		 aps.primary_genre,
+		 psa.genres,
+		 psa.install_count,
+		 aps.review_count,
+		 psa.review_count
+ORDER BY avg_rating DESC,
+		 install_count DESC,
+		 purchase_price,
+		 monthly_revenue DESC
+-------------------------------------------------------------------------
 --1) gathering names from both tables in alphabetical order
-SELECT a.name
+SELECT
+		a.name
 FROM app_store_apps AS a
 INNER JOIN play_store_apps AS p
-	ON a.name=p.name
-	ORDER by name ASC
+USING(name)
 -------------------------------------------------------------------------
 --2) Highest review count using previous quesry
 
@@ -123,6 +172,13 @@ FROM app_store_apps AS a
 		ON a.name=p.name
 		GROUP BY a.name
 ORDER by avg_rating DESC, avg_count DESC
+-----------------------------------------------------------------------------
+SELECT a.name, p.name, a.rating, p.rating
+FROM app_store_apps AS a
+INNER JOIN play_store_apps AS p
+USING(name)
+WHERE name ilike '%instagram%'
+GROUP BY a.name, p.name, a.rating, p.rating
 ------------------------------------------------------------------------------
 --4)ADD genre (combine diff column names of primary_genre and genre)
 
@@ -137,7 +193,7 @@ FROM app_store_apps AS a
 GROUP BY a.name, a.primary_genre
 --ORDER by avg_rating DESC--, avg_count DESC
 -------------------------------------------------------------------------------
---Find total number of genre categories
+--5Find total number of genre categories
 SELECT
 		a.primary_genre
 	,	COUNT(a.primary_genre) AS total_genre	
@@ -146,7 +202,7 @@ GROUP BY a.primary_genre
 ORDER BY total_genre DESC
 
 -------------------------------------------------------------------------------
---Find percentage with last formula using convert
+--6Find percentage
 SELECT
 		a.primary_genre
 	,	COUNT(a.primary_genre) AS total_genre
@@ -154,60 +210,32 @@ SELECT
 FROM app_store_apps AS a
 GROUP BY a.primary_genre
 ORDER BY total_genre DESC
+-------------------------------------------------------------------------------
+--7find top profitability using case (recommended/not recommended)
+
+
+		
 
 
 --------------------------------------------------------------------------------
---5) create a table showing count of each category and percentage of overall genre
-
-SELECT
-		a.primary_genre
-	,	COUNT(*) AS genre_count
-	,	ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (), 2) AS genre_percentage
-FROM 
-	app_store_apps AS a
-GROUP BY
-	a.primary_genre
-ORDER BY
-	genre_count DESC
-------------------------------------------------------------------------------
---6) combine the last two
-SELECT 
-		a.name
-	,	a.primary_genre
-	,	ROUND(AVG((CAST(a.review_count AS INTEGER) + CAST(p.review_count AS INTEGER))/2.0),2) AS avg_count
-	,	ROUND(AVG((a.rating + p.rating)/2),0) AS avg_rating
-	,	a.primary_genre
-	,	COUNT(*) AS genre_count
-	,	ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (), 2) AS genre_percentage
+SELECT COUNT(*),  a.name,p.name
 FROM app_store_apps AS a
-	INNER JOIN play_store_apps AS p
-		ON a.name=p.name
-GROUP BY a.name, a.primary_genre
-ORDER by avg_rating DESC, avg_count DESC, genre_count DESC
-------------------------------------------------------------------------------
---7)add the price of 0 OR 1 using case 
+INNER JOIN play_store_apps AS p
+USING(name)
+GROUP BY a.name, p.name
+ORDER by a.name, p.name
 
-SELECT
-		ROUND(AVG((CAST(REPLACE(a.price::TEXT, '$', '') AS NUMERIC) + CAST(REPLACE(p.price::TEXT, '$', '') AS NUMERIC))/2),2)AS avg_price
-	,	a.name
-	,	a.primary_genre
-	,	ROUND(AVG((CAST(a.review_count AS INTEGER) + CAST(p.review_count AS INTEGER))/2.0),2) AS avg_count
-	,	ROUND(AVG((a.rating + p.rating)/2),0) AS avg_rating
-	,	a.primary_genre
-	,	COUNT(*) AS genre_count
-	,	ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (), 2) AS genre_percentage
-	,	(CASE WHEN ROUND(AVG((CAST(REPLACE(a.price::TEXT, '$', '') AS NUMERIC) + CAST(REPLACE(p.price::TEXT, '$', '') AS NUMERIC))/2),2)<1 THEN 10000
-			   WHEN ROUND(AVG((CAST(REPLACE(a.price::TEXT, '$', '') AS NUMERIC) + CAST(REPLACE(p.price::TEXT, '$', '') AS NUMERIC))/2),2)<2 THEN 10000
-			   ELSE NULL END) AS total_price
-FROM app_store_apps AS a
-	INNER JOIN play_store_apps AS p
-		ON a.name=p.name
-GROUP BY a.name, a.primary_genre
-ORDER by avg_rating DESC, avg_count DESC, genre_count DESC
-
-
-
-
-
+------------------------------------------------------------------------------------
+(SELECT name,ROUND(AVG(rating),1) as avg_rating,CAST(price AS NUMERIC) AS price, CAST(review_count as integer) as review_count
+    FROM app_store_apps AS a
+	WHERE rating > 4.5
+	GROUP BY content_rating,name,rating,price,review_count)
+    UNION ALL
+(SELECT name,ROUND(AVG(rating),1) avg_rating,CAST(REPLACE(price, '$', '') AS DECIMAL) AS price, Review_count
+    FROM play_store_apps AS p
+	WHERE rating > 4.5)
+	GROUP BY content_rating,name,rating,price,review_count)
+	ORDER BY avg_rating DESC, review_count DESC
+	LIMIT  15
 
 
